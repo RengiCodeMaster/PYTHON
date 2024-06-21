350896e93cc8c3c8811a2d1b868b098f3687a324
Brennis Castro <castrocanobrennis@gmail.com>
2024-06-21 07:47:01 -0500

    mejorado

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def prosefores():
     app.title("Tutorias")
     app.geometry("600x300")
 
-    # Declaramos las variables para interacturar con la base de datos
+    # Declaramos las variables para interactuar con la base de datos
 
     Codigo = StringVar()
     Nombre = StringVar()
@@ -18,34 +18,34 @@ def prosefores():
     Genero = StringVar()
     Horario = StringVar()
 
-    marco = Label(app, text="Seleccion de tutoria")
-    marco.place(x=10, y=10, width=500, height=400)
-    marco.config(bg="lightblue")
+    # Crea el marco en la parte superior
+    marco = LabelFrame(app, text="Seleccion de tutoria", bg="lightblue")
+    marco.place(x=10, y=10, width=580, height=250)
 
     # Crea los elementos del marco
-    lbCodigo = Label(marco, text="Codigo").grid(column=0, row=2, padx=5, pady=5)
+    lbCodigo = Label(marco, text="Codigo").grid(column=0, row=0, padx=5, pady=5)
     txtCodigo = Entry(marco, textvariable=Codigo)
-    txtCodigo.grid(column=1, row=2, padx=5, pady=5)
+    txtCodigo.grid(column=1, row=0, padx=5, pady=5)
 
-    lbNombre = Label(marco, text="Nombre").grid(column=0, row=3, padx=5, pady=5)
+    lbNombre = Label(marco, text="Nombre").grid(column=0, row=1, padx=5, pady=5)
     txtNombre = Entry(marco, textvariable=Nombre)
-    txtNombre.grid(column=1, row=3, padx=5, pady=5)
+    txtNombre.grid(column=1, row=1, padx=5, pady=5)
 
-    lbApellido = Label(marco, text="Apellido").grid(column=0, row=4, padx=5, pady=5)
+    lbApellido = Label(marco, text="Apellido").grid(column=0, row=2, padx=5, pady=5)
     txtApellido = Entry(marco, textvariable=Apellido)
-    txtApellido.grid(column=1, row=4, padx=5, pady=5)
+    txtApellido.grid(column=1, row=2, padx=5, pady=5)
 
-    lbEmail = Label(marco, text="Email").grid(column=2, row=2, padx=5, pady=5)
+    lbEmail = Label(marco, text="Email").grid(column=2, row=0, padx=5, pady=5)
     txtEmail = Entry(marco, textvariable=Email)
-    txtEmail.grid(column=3, row=2, padx=5, pady=5)
+    txtEmail.grid(column=3, row=0, padx=5, pady=5)
 
-    lbGenero = Label(marco, text="Genero").grid(column=2, row=3, padx=5, pady=5)
+    lbGenero = Label(marco, text="Genero").grid(column=2, row=1, padx=5, pady=5)
     txtGenero = Entry(marco, textvariable=Genero)
-    txtGenero.grid(column=3, row=3, padx=5, pady=5)
+    txtGenero.grid(column=3, row=1, padx=5, pady=5)
 
-    lbHorario = Label(marco, text="Horario").grid(column=2, row=4, padx=5, pady=5)
+    lbHorario = Label(marco, text="Horario").grid(column=2, row=2, padx=5, pady=5)
     txtHorario = Entry(marco, textvariable=Horario)
-    txtHorario.grid(column=3, row=4, padx=5, pady=5)
+    txtHorario.grid(column=3, row=2, padx=5, pady=5)
 
     app.mainloop()
 
